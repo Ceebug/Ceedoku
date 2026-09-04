784e734297d8ac4234068e8273010195e7821f7a
Ceebug <caseydegan@outlook.com>
2026-09-04 21:36:47 +1000

    Change language output format to include HTML line breaks
    Update language percentages format in update-languages.js

--- .github/scripts/update-languages.js
+++ .github/scripts/update-languages.js
@@ -78,8 +78,8 @@ if (start === -1 || end === -1 || end < start) {
     throw new Error("LANGUAGES_START or LANGUAGES_END marker not found.");
 }
 
-const languages = `- JavaScript - ${percentages.JavaScript}%
-- CSS - ${percentages.CSS}%
+const languages = `- JavaScript - ${percentages.JavaScript}%<br>
+- CSS - ${percentages.CSS}%<br>
 - HTML - ${percentages.HTML}%`;
 
 const updatedReadme =
